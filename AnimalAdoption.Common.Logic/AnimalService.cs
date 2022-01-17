881eed0162884753a7cfc1454ae7730c6903bc69
Kishor Rani <kishor.rani@awaregroup.com>
2022-01-18 11:24:38 +1300

    test

--- AnimalAdoption.Common.Logic/AnimalService.cs
+++ AnimalAdoption.Common.Logic/AnimalService.cs
@@ -9,7 +9,7 @@ namespace AnimalAdoption.Common.Logic
         public Animal[] ListAnimals => new Animal[] {
             new Animal { Id = 1, Name = "Sedi", Age = 50, Description = "Soft natured" },
             new Animal { Id = 2, Name = "Metamorph", Age = 50, Description = "Under a lot of pressure" },
-            new Animal { Id = 3, Name = "Igno", Age = 50, Description = "Shiny and glasslike" },
+            new Animal { Id = 3, Name = "Kishor", Age = 25, Description = "Learning Git" },
         };
     }
 }
